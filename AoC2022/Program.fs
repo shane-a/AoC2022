9e51d414613a7aaf8fb0ba1f33d9a179b2202813
Shane Adam <shanea@pandell.com>
2022-12-09 08:29:04 -0700

    cleanups

--- AoC2022/Program.fs
+++ AoC2022/Program.fs
@@ -6,21 +6,25 @@ open System.IO
 let d1p1 () = AoC2022.Helpers.FileHelpers.readFileGroupByDoubleNewLineToInts("../../../Day1Input.txt")
            |> Seq.map(fun g -> g |> Seq.sum)
            |> Seq.max
+           |> string
            
 let d1p2 () = AoC2022.Helpers.FileHelpers.readFileGroupByDoubleNewLineToInts("../../../Day1Input.txt")
             |> Seq.map(fun g -> g |> Seq.sum)
             |> Seq.sortDescending
             |> Seq.take 3
             |> Seq.sum
+            |> string
             
 let d2p1 () = AoC2022.Helpers.FileHelpers.readFileSplitToPairs("../../../Day2Input.txt")
             |> Seq.map(fun pair -> AoC2022.DayHelpers.Day2.calcRoundScore(pair[0], pair[1]))
             |> Seq.sum
+            |> string
             
 let d2p2 () = AoC2022.Helpers.FileHelpers.readFileSplitToPairs("../../../Day2Input.txt")
             |> Seq.map(fun pair -> [|pair[0]; AoC2022.DayHelpers.Day2.calcRequiredMove(pair[0], pair[1])|])
             |> Seq.map(fun pair -> AoC2022.DayHelpers.Day2.calcRoundScore(pair[0], pair[1]))
             |> Seq.sum
+            |> string
             
 let d3p1 () = AoC2022.Helpers.FileHelpers.readFileByLine("../../../Day3Input.txt")
             |> Seq.map(AoC2022.DayHelpers.Day3.splitStringInHalf)
@@ -28,6 +32,7 @@ let d3p1 () = AoC2022.Helpers.FileHelpers.readFileByLine("../../../Day3Input.txt
             |> Seq.map(fun s -> Set.toArray(s)[0])
             |> Seq.map(AoC2022.DayHelpers.Day3.getItemScore)
             |> Seq.sum
+            |> string
             
 let d3p2 () = AoC2022.Helpers.FileHelpers.readFileByLine("../../../Day3Input.txt")
             |> Array.chunkBySize 3
@@ -35,6 +40,7 @@ let d3p2 () = AoC2022.Helpers.FileHelpers.readFileByLine("../../../Day3Input.txt
             |> Seq.map(fun s -> Set.toArray(s)[0])
             |> Seq.map(AoC2022.DayHelpers.Day3.getItemScore)
             |> Seq.sum
+            |> string
 
 let d4p1 () = AoC2022.Helpers.FileHelpers.readFileByLine("../../../Day4Input.txt")
             |> Seq.map (fun i -> i.Split(','))
@@ -42,6 +48,7 @@ let d4p1 () = AoC2022.Helpers.FileHelpers.readFileByLine("../../../Day4Input.txt
             |> Seq.map Array.ofSeq
             |> Seq.filter(fun i -> (i[0][0] <= i[1][0] && i[0][1] >= i[1][1]) || (i[0][0] >= i[1][0] && i[0][1] <= i[1][1]) )
             |> Seq.length
+            |> string
             
 let d4p2 () = AoC2022.Helpers.FileHelpers.readFileByLine("../../../Day4Input.txt")
             |> Seq.map (fun i -> i.Split(','))
@@ -50,6 +57,7 @@ let d4p2 () = AoC2022.Helpers.FileHelpers.readFileByLine("../../../Day4Input.txt
             |> Seq.map(fun i -> [| seq{i[0][0] .. i[0][1]}; seq{i[1][0] .. i[1][1]} |])
             |> Seq.filter(fun i -> Set.intersect (Set.ofSeq(i[0])) (Set.ofSeq(i[1])) |> Set.isEmpty |> not )
             |> Seq.length
+            |> string
             
 let d5p1 () =
     let input = AoC2022.Helpers.FileHelpers.readFileSplitOnDoubleNewLine("../../../Day5Input.txt")
@@ -93,10 +101,10 @@ let main _ =
     let input = Console.ReadLine()
                 |> int
     let p1, p2 = match input with
-                    | 1 -> (d1p1() |> string, d1p2() |> string)
-                    | 2 -> (d2p1() |> string, d2p2() |> string)
-                    | 3 -> (d3p1() |> string, d3p2() |> string)
-                    | 4 -> (d4p1() |> string, d4p2() |> string)
+                    | 1 -> (d1p1(), d1p2())
+                    | 2 -> (d2p1(), d2p2())
+                    | 3 -> (d3p1(), d3p2())
+                    | 4 -> (d4p1(), d4p2())
                     | 5 -> (d5p1(), d5p2())
                     | 6 -> (d6p1(), d6p2())
                     | _ -> raise (InvalidOperationException($"Day %d{input} has no functions"))
